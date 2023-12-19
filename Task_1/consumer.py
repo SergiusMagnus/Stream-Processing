a6c94641e7235bf2792040941ec9439f57af573b
Sergei <sergeypost1946@gmail.com>
2023-12-19 21:24:22 +0300

    Task_1 update.

--- Task_1/consumer.py
+++ Task_1/consumer.py
@@ -13,6 +13,17 @@ config = {
     'auto.offset.reset': 'earliest'
 }
 
+
+def print_stats(data: list[dict]):
+    df = pd.DataFrame(data)
+
+    print('Mean by sensor name:')
+    print(df.groupby('name')['value'].mean().to_string(header=False))
+          
+    print('\nMean by sensor type:')
+    print(df.groupby('type')['value'].mean().to_string(header=False))
+
+
 def main(delay: float) -> NoReturn:
     consumer = Consumer(config)
     consumer.subscribe([TOPIC_NAME])
@@ -24,23 +35,18 @@ def main(delay: float) -> NoReturn:
         while True:
             msg = consumer.poll(1)
 
-            if msg is None:
-                continue
-
-            msgs.append(json.loads(msg.value()))
+            if msg:
+                msgs.append(json.loads(msg.value()))
 
             if datetime.now() > end_time:
                 print('\n', '=' * 50, '\n')
 
-                df = pd.DataFrame.from_dict(msgs)
+                if msgs:
+                    print_stats(msgs)
+                    msgs = []
+                else:
+                    print('No data.')
 
-                print('Mean by sensor name:')
-                print(df.groupby('name')['value'].mean().to_string(header=False))
-
-                print('\nMean by sensor type:')
-                print(df.groupby('type')['value'].mean().to_string(header=False))
-
-                msgs = []
                 end_time = datetime.now() + timedelta(seconds=delay)
 
     finally:
